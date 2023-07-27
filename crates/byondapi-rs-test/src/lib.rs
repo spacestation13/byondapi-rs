90a7f5dafabe6659aba7093b23b99215b44545b0
tigercat2000 <nick.pilant2@gmail.com>
2023-07-26 19:15:16 -0700

    Promote parse_args to a real library function

--- crates/byondapi-rs-test/src/lib.rs
+++ crates/byondapi-rs-test/src/lib.rs
@@ -1,28 +1,24 @@
 #![allow(clippy::missing_safety_doc)]
 
-use byondapi::value::ByondValue;
-
-fn parse_args(argc: byondapi_sys::u4c, argv: *const ByondValue) -> &'static [ByondValue] {
-    unsafe { std::slice::from_raw_parts(argv, argc as usize) }
-}
+use byondapi::{parse_args, value::ByondValue};
 
 #[no_mangle]
 pub unsafe extern "C" fn test_connection(
     _argc: byondapi_sys::u4c,
-    _argv: *const ByondValue,
+    _argv: *mut ByondValue,
 ) -> ByondValue {
     ByondValue::new_num(69.0)
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn test_args(argc: byondapi_sys::u4c, argv: *const ByondValue) -> ByondValue {
+pub unsafe extern "C" fn test_args(argc: byondapi_sys::u4c, argv: *mut ByondValue) -> ByondValue {
     let args = parse_args(argc, argv);
     assert_eq!(args.len(), 1);
     args[0].clone()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn send_test(argc: byondapi_sys::u4c, argv: *const ByondValue) -> ByondValue {
+pub unsafe extern "C" fn send_test(_argc: byondapi_sys::u4c, _argv: *mut ByondValue) -> ByondValue {
     // let args = parse_args(argc, argv);
     let new_value = ByondValue::new_str("Meow").unwrap();
 
--- crates/byondapi-rs/src/lib.rs
+++ crates/byondapi-rs/src/lib.rs
@@ -11,3 +11,14 @@ pub mod list;
 pub mod prelude;
 pub mod typecheck_trait;
 pub mod value;
+
+use crate::value::ByondValue;
+/// # Safety
+/// Don't pass in a null argv pointer please god
+/// Just give this what BYOND gives you and pray for the best
+pub unsafe fn parse_args(
+    argc: byondapi_sys::u4c,
+    argv: *mut ByondValue,
+) -> &'static mut [ByondValue] {
+    unsafe { std::slice::from_raw_parts_mut(argv, argc as usize) }
+}
