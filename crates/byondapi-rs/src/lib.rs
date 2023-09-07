bb0d9d73b6199183f910367fba7bf8625432b8fc
Katherine Kiefer <sterlite2902@gmail.com>
2023-09-07 20:50:42 +1000

    to_vec it

--- crates/byondapi-rs/src/lib.rs
+++ crates/byondapi-rs/src/lib.rs
@@ -15,11 +15,8 @@ use crate::value::ByondValue;
 /// # Safety
 /// Don't pass in a null argv pointer please god
 /// Just give this what BYOND gives you and pray for the best
-pub unsafe fn parse_args(
-    argc: byondapi_sys::u4c,
-    argv: *mut ByondValue,
-) -> &'static mut [ByondValue] {
-    unsafe { std::slice::from_raw_parts_mut(argv, argc as usize) }
+pub unsafe fn parse_args(argc: byondapi_sys::u4c, argv: *mut ByondValue) -> Vec<ByondValue> {
+    unsafe { std::slice::from_raw_parts_mut(argv, argc as usize).to_vec() }
 }
 
 /// Re-export of byondapi_sys for all low level things you may run into.
