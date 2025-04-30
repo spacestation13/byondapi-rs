c4393855134744e95ab935cc4598f70824ff9447
Katherine Kiefer <sterlite2902@gmail.com>
2025-04-30 21:45:48 +1000

    redo it

--- crates/byondapi-rs/src/runtime.rs
+++ crates/byondapi-rs/src/runtime.rs
@@ -1,11 +1,12 @@
 use crate::static_global::byond;
 use std::ffi::CString;
 
-/// Immediately returns a runtime from this context
+/// Immediately returns a runtime from this context.
 ///
-/// # WARNING, THIS WILL NOT CALL DROP ON THINGS, YOU MUST DROP EVERYTHING BEFORE YOU CALL THIS FUNCTION
+/// ## This function will immediately longjump to byond, Drop destructors or catch_unwind will be ignored.
+/// ## Make sure you drop everything before you call this.
 pub unsafe fn byond_runtime<S: Into<Vec<u8>>>(message: S) -> ! {
     let c_str = CString::new(message.into()).unwrap();
     unsafe { byond().Byond_CRASH(c_str.as_ptr()) };
-    panic!()
+    unreachable!()
 }
